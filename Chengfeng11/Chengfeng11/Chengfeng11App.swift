cfa535f30284e017dac46ebb7622d40918c89d0a
ruanjianyingyongbu <luyajun.shgbit.com>
2021-02-26 12:44:49 +0800

    锋哥带我

---
+++ Chengfeng11/Chengfeng11/Chengfeng11App.swift
@@ -0,0 +1,17 @@
+//
+//  Chengfeng11App.swift
+//  Chengfeng11
+//
+//  Created by ruanjianyingyongbu on 2021/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Chengfeng11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chengfeng11/Chengfeng11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chengfeng11
+//
+//  Created by ruanjianyingyongbu on 2021/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
